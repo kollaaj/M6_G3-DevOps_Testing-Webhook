cfef47c5823f5bd3196038214f5972aee5b5b2ae
Kollaaj <kolbrunaj@gmail.com>
2021-03-08 23:47:50 +0000

    Changed webhook route to /

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import json
 
 app = Flask(__name__)
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/webhook', methods=['POST'])
+@app.route('/', methods=['POST'])
 def webhook():
     data = request.get_json()
     data_str = json.dumps(data)
