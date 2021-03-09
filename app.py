b5e73829548b647ccf3c693a7a8933f7670523b2
Kollaaj <kolbrunaj@gmail.com>
2021-03-09 01:25:54 +0000

    Added trigger table to HTML

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
+from datetime import datetime
 import pymongo
-import json
 import os
 
 app = Flask(__name__)
@@ -12,10 +12,12 @@ mongo_client = pymongo.MongoClient(mongo_uri)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    triggers = list(mongo_client['hook']['triggers'].find({ 'action': { '$ne': 'started' } }).sort('_id', -1).limit(100))
+    return render_template('index.html', triggers=triggers)
 
 @app.route('/', methods=['POST'])
 def webhook():
-    data = request.get_json()
-    mongo_client['hook']['triggers'].insert_one(data)
+    trigger = request.get_json()
+    trigger['my_timestamp'] = datetime.utcnow().strptime('%A %d %B %Y at %H:%M:%S')
+    mongo_client['hook']['triggers'].insert_one(trigger)
     return ('', 204)
